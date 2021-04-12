c3a108befec466840a8ff50ed3e0a95ca0ebee42
wildheart007 <cang.b.le@gmail.com>
2021-04-12 11:04:30 -0700

    dart sass config

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
+    //'@storybook/preset-create-react-app', // react-app includes sass-loader into its package, therefore react-app is not necessary since line-26 is configed w/ sass-laoder.
   ],
 
   webpackFinal: async (config, { configType }) => {
@@ -18,11 +18,7 @@ module.exports = {
         rules: [
           {
             test: /\.scss$/,
-            include: [
-              path.resolve(process.cwd(), 'src'),
-              path.resolve(process.cwd(), 'node_modules'),
-              path.resolve(process.cwd(), '.storybook'),
-            ],
+            //use: ['style-loader', 'css-loader', 'sass-loader' ],
             use: [
               'style-loader',
               {
@@ -38,9 +34,61 @@ module.exports = {
                 },
               },
             ],
+            // include: [
+            //   //path.resolve(process.cwd(), 'src'),
+            //   //path.resolve(process.cwd(), 'node_modules'),
+            //   //path.resolve(process.cwd(), '.storybook'),
+            // ],
           },
         ],
       },
     });
   },
 };
+
+// const path = require('path');
+// const { merge } = require('webpack-merge');
+
+// module.exports = {
+//   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/preset-create-react-app',
+//   ],
+
+//   webpackFinal: async (config, { configType }) => {
+//     return merge(config, {
+//       resolve: {
+//         modules: ['src', '.storybook'],
+//       },
+//       module: {
+//         rules: [
+//           {
+//             test: /\.scss$/,
+//             include: [
+//               path.resolve(process.cwd(), 'src'),
+//               path.resolve(process.cwd(), 'node_modules'),
+//               path.resolve(process.cwd(), '.storybook'),
+//             ],
+//             use: [
+//               'style-loader',
+//               {
+//                 loader: 'css-loader',
+//                 options: { modules: true },
+//               },
+//               {
+//                 loader: 'sass-loader',
+//                 options: {
+//                   sassOptions: {
+//                     includePaths: [path.resolve(process.cwd(), 'node_modules')],
+//                   },
+//                 },
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//     });
+//   },
+// };
--- src/components/button/ButtonComponent.jsx
+++ src/components/button/ButtonComponent.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import cn from 'classnames';
-import css from './Button.scss';
+import css from './Button.module.scss';
 
 export default function ButtonComponent(props) {
   const { color, size, children, disabled, block, loading } = props;
-  const classes = cn('root', {
-    [color]: color,
-    [size]: size,
-    block: block,
-    loading: loading,
-  });
+  const classes = cn(
+    css.root,
+    { [css[color]]: color },
+    { [css[size]]: size },
+    { [css.block]: block },
+    { [css.loading]: loading }
+  );
 
   return (
-    <div className='button-container'>
+    <div className={css['button-container']}>
       <button className={classes} disabled={disabled}>
         {children}
       </button>
